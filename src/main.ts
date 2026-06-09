68bf3f00de0e0316b4f9d70f1794417a561a26d0
Mateo-Moreno <mateomorenolopez09@gmail.com>
2026-06-09 11:45:04 -0500

    fix main

--- src/main.ts
+++ src/main.ts
@@ -12,38 +12,31 @@ import {
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule,
-  );
+  const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-
       forbidNonWhitelisted: true,
-
       transform: true,
     }),
   );
 
   app.useGlobalFilters(
-  new HttpExceptionFilter(),
-);
+    new HttpExceptionFilter(),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Company Employees API')
-    .setDescription(
-      'API con Onion Architecture',
-    )
+    .setDescription('API con Onion Architecture')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
 
-  const document =
-    SwaggerModule.createDocument(
-      app,
-      config,
-    );
+  const document = SwaggerModule.createDocument(
+    app,
+    config,
+  );
 
   SwaggerModule.setup(
     'swagger',
@@ -51,7 +44,11 @@ async function bootstrap() {
     document,
   );
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
+
+  console.log(`Application running on port ${port}`);
 }
 
 bootstrap();
